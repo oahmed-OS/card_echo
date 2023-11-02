af44717ec2317e268c86a28416a7ae3e8384ff38
Omar Ahmed <omarahmed1121@gmail.com>
2023-11-01 23:13:55 -0500

    test no base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: `https://oahmed-os.github.io`,
-  base: '/card_echo',
   integrations: [react()]
 });
